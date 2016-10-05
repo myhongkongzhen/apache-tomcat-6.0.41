072ca95a15b91c57fde2698bd716c8a54164dd8b
Z_Z.W <myhongkongzhen@gmail.com>
2016-10-05 10:16:29 +0800

    init tomcat 6.0.41

---
+++ callnexus.bat
@@ -0,0 +1,7 @@
+set CATALINA_BASE=D:\01_opensource\programmes\apache-tomcat-6.0.41
+
+set CATALINA_HOME=D:\01_opensource\programmes\apache-tomcat-6.0.41
+
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79
+
+call D:\01_opensource\programmes\apache-tomcat-6.0.41\bin\service.bat install callnexus 
